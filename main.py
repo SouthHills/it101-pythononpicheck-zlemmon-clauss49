f3902cea253105cac6ec749186957a1324ab8df7
Nicholas Page <npage@southhills.edu>
2024-03-06 13:41:50 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+yourName = "[YOUR NAME HERE]"
+yourSouthHillsUserName = "[YOUR USERNAME HERE]"
+
+print(f"Hello, {yourName} | {yourSouthHillsUserName}")
+
+verificationNum = random.randint(1, 99)
+
+try:
+    userNumber = int(input(f"Enter the number {verificationNum} in your terminal: "))
+
+    if (verificationNum == userNumber):
+        print("Great! You're all done this lab.")
+    else:
+        print("Please run the program again and enter the correct input.")
+except Exception as e:
+    print("Error!")
+    print(e)

3313204efd28ef7ed9cfacf1864ebe4831b44718
SH-NicholasPage <npage@southhills.edu>
2024-03-06 14:09:56 -0500

    Text change

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ try:
     userNumber = int(input(f"Enter the number {verificationNum} in your terminal: "))
 
     if (verificationNum == userNumber):
-        print("Great! You're all done this lab.")
+        print("Great! You're done with this lab.")
     else:
         print("Please run the program again and enter the correct input.")
 except Exception as e:
